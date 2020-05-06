c71ab2a124a3903a63f1dccfe83f919f40415f4b
Arique Aguilar <arique1104@gmail.com>
2020-05-06 14:28:02 -0500

    Independent Challenge 2005 050620 Arique

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+class School
+attr_reader :start_time, :hours_in_school_day, :student_names
 
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    @start_time
+    #must find a way to seperate that first digit and leave the :00 alone.
+
+
+  end
+
+  def standard_student_names
+    #accumulator?
+    # standard_student_names = []
+    @student_names.each do |n|
+      standard_student_names << n.capitalize
+    end
+  end
+
+end
